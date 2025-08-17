d11c156f295dadfcd47a5a215a3fdb14a3d69975
AMEC17 <129676081+AMEC17@users.noreply.github.com>
2025-08-17 23:43:46 +0800

    refactored column into its own component

---
+++ src/components/Board/Column.tsx
@@ -0,0 +1,59 @@
+import { MdOutlineModeEditOutline } from "react-icons/md"
+import { Column, Items } from "@/utilities/exports"
+import Task from "./Task"
+
+interface ColumnProps {
+  handleDragOver: (e: React.DragEvent<HTMLDivElement>) => void
+  handleDrop: (e: React.DragEvent<HTMLDivElement>, columnId: number) => void
+  columns: Column[]
+  index: number
+  handleEditColumn: (columnID: number) => void,
+  removeColumn: (columnName: string) => void,
+  handleDragStart: (columnID: number, item: Items) => void,
+  removeTask: (columnID: number, taskID: string) => void
+}
+
+export default function ColumnComponent(
+  { handleDragOver, handleDrop, columns, index, handleEditColumn, removeColumn, handleDragStart, removeTask }: ColumnProps) {
+  return (
+    <div
+      className="flex flex-col shrink-0 w-80 bg-zinc-800 rounded-lg shadow-xl"
+      onDragOver={(e) => handleDragOver(e)}
+      onDrop={(e) => handleDrop(e, index)}>
+
+      <div
+        className={`flex p-4 text-white font-bold text-xl rounded-t-md w-full bg-gradient-to-r ${columns[index].from} ${columns[index].to}`}>
+        {columns[index].name}
+        <span className="ml-2 px-2 py-1 bg-zinc-800 bg-opacity-30 rounded-full text-sm">{columns[index].items.length}</span>
+
+        <button
+          onClick={() => handleEditColumn(index)}
+          className="ml-auto text-zinc-400 hover:text-red-400 transform-colors duration-200
+                                w-8 h-8 flex items-center justify-center rounded-full hover:bg-zinc-600">
+          <MdOutlineModeEditOutline className="text-2xl cursor-pointer" />
+        </button>
+
+        <button
+          onClick={() => removeColumn(columns[index].name)}
+          className="text-zinc-400 hover:text-red-400 transform-colors duration-200
+                                w-8 h-8 flex items-center justify-center rounded-full hover:bg-zinc-600">
+          <span className="text-2xl cursor-pointer">x</span>
+        </button>
+      </div>
+
+      <div className="p-3 min-h-64">
+        {columns[index].items.length === 0 ? (
+          <div className="text-center py-10 text-zinc-500 italic text-sm"> Drop Tasks Here</div>
+        ) : (
+          columns[index].items.map((item) => (
+            <Task key={item.id}
+              index={index}
+              item={item}
+              handleDragStart={handleDragStart}
+              removeTask={removeTask} />
+          ))
+        )}
+      </div>
+    </div>
+  )
+}
